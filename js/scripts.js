9a8fbda4d43ab81974e852573e1f295e73ddb036
Joanna Kasperek <joastr1@gmail.com>
2018-11-29 11:19:01 +0100

    add js file

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function Phone(brand, price, color, screen, giga) {
+	this.brand = brand;
+	this.price = price;
+    this.color = color;
+    this.screen = screen;
+    this.giga = giga;
+}
+Phone.prototype.printInfo = function() {
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", screen is " + this.screen + " and the price is " + this.price + "." + " Additionally, this price include " + this.giga + " Internet."  );
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver", "6'", "2GB");
+var OnePlusOne = new Phone("Never Settle", 1700, "black", "7'", "10GB" );
+var SamsungGalaxyS6 = new Phone("Samsung", 2500, "gold", "6'", "5GB");
+
+iPhone6S.printInfo();
+OnePlusOne.printInfo();
+SamsungGalaxyS6.printInfo();
+
+
+
